ccce77504fff31f3bdaf50c3680f2411805c7b8b
Toshiaki Kawabata <toshiaki@techsoft3d.com>
2025-02-06 13:17:07 +0900

    Ported 2025.1.0 and Ps35.1

--- _VS2019_HEPs.bat
+++ _VS2019_HEPs.bat
@@ -1,5 +1,5 @@
-SET HVISUALIZE_INSTALL_DIR=C:\SDK\Visualize\HOOPS_Visualize_2024
-SET HEXCHANGE_INSTALL_DIR=C:\SDK\Exchange_Publish\HOOPS_Exchange_Publish_2024
-SET PARASOLID_INSTALL_DIR=C:\SDK\Parasolid\v34.1\kernel
+SET HVISUALIZE_INSTALL_DIR=C:\SDK\Visualize\HOOPS_Visualize_2025.1.0
+SET HEXCHANGE_INSTALL_DIR=C:\SDK\Exchange_Publish\HOOPS_Exchange_Publish_2025.1.0
+SET PARASOLID_INSTALL_DIR=C:\SDK\Parasolid\v35.1\kernel
 
 CALL "C:\Program Files (x86)\Microsoft Visual Studio\2019\Professional\Common7\IDE\devenv.exe" hps_mfc_sandbox_HEPs_v142.sln
